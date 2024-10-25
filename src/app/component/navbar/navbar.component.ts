808c035a222e0496c5e37c71883f0801687c5a1b
edeolazabal-upc <PCSIEDDE@upc.edu.pe>
2024-10-25 23:47:28 +0000

    edicion de mavbar y lista

---
+++ src/app/component/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, RouterModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
